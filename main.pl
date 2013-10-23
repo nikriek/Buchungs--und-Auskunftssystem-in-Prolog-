c8005e6a6a06fa77c7e56af8cecbb1c8d0db77ed
Niklas Riekenbrauck <nikriek@users.noreply.github.com>
2013-10-23 15:19:40 +0200

    Update main.pl

--- main.pl
+++ main.pl
@@ -31,9 +31,39 @@ terminierung:- titel,
 	write(' Kunden...'), rename_file('frkunden.pl', 'frkunden.bak'), tell('frkunden.pl'), listing(kunde), told, writeln(' ok!'),
 	write(' Buchungen...'), rename_file('frbuchen.pl', 'frbuchen.bak'), tell('frbuchen.pl'), listing(buchung), told, writeln(' ok!'),
 	retractall(wahl(_)).
+
+
+wahl_ausfuehren(0'1):-
+	writeln(' Urlaubsgebiete'), nl, gebiete_zeigen,
+	gebiet_waehlen.
 	
+wahl_ausfuehren(0'2):-
+	writeln(' Hotels'), nl, linksbuendig('Nummer', 9), linksbuendig('Name', 15), linksbuendig('Kategorie', 10), linksbuendig('Gebiet', 10), nl, linie_zeichnen('-', 40), nl, hotels_zeigen,
+	hotel_waehlen, weiter.
+	
+wahl_ausfuehren(0'3):- 
+	writeln(' Kunden'), nl, write('Name oder Nummer: '), lese_string(Kunde), bearbeite_kunde(Kunde), weiter.
+
+wahl_ausfuehren(0'4):- 
+	writeln(' Buchen'), nl, hole_kunde(Kunde), hole_hotel(Hotel), buchen(Hotel, Kunde).
+
+wahl_ausfuehren(0'5):- 
+	writeln(' neuer Kunde'), nl, retractall(wahl(_)), asserta(wahl(gebiet(_))), asserta(wahl(hotel(_))), asserta(wahl(kunde(_))).
+
+wahl_ausfuehren(_).
+
 % Gebietsverwaltung
 
+gebiete_zeigen:-
+	findall(Gebiet, hotel(_, _, _, Gebiet), Liste1), 
+	sort(Liste1, Liste2),
+	gebiete_zeigen(Liste2),
+	
+gebiete_zeigen([K|R]):-
+	tab(2), writeln(K), 
+	gebiete_zeigen(R).
+	gebiete_zeigen([]).
+	
 % Hotelverwaltung
 
 % Kundenverwaltung
