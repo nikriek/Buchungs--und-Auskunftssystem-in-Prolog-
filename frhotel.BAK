a44722887419f862d4de79d15ad67f90226263dc
RexUmbrarum <20england11@gmail.com>
2013-10-27 16:43:46 +0100

    Programm funktioniert jetzt
    Datensätze hinzugefügt (Hotels, Buchungen, Kunden).
    Kundenverwaltung hinzugefügt
    Buchungsverwaltung hinzugefügt
    Quellcodeübersicht etwas verbessert

---
+++ frhotel.BAK
@@ -0,0 +1,8 @@
+% Autor: Georg Zänker, Niklas Rieckenbrauck, Jonas Umland
+% Datum: 27.10.2013
+
+kunde(0101,'Plum', 'Untergasse 4',64285,'Darmstadt').
+kunde(0102,'Rettig', 'Odenwaldstr. 17',64293,'Darmstadt').
+kunde(0104,'Gaydoul', 'Auf der Beune 8',64807,'Dieburg').
+kunde(0105,'Laaber', 'Kochstr. 23a',64291,'Darmstadt').
+kunde(0113,'Bozdag', 'Kostheimerstr. 18',64354,'Reinheim').
---
+++ frkunden.pl
@@ -0,0 +1,8 @@
+% Autor: Georg Zänker, Niklas Rieckenbrauck, Jonas Umland
+% Datum: 27.10.2013
+
+kunde(101, 'Plum', 'Untergasse 4', 64285, 'Darmstadt').
+kunde(102, 'Rettig', 'Odenwaldstr. 17', 64293, 'Darmstadt').
+kunde(104, 'Gaydoul', 'Auf der Beune 8', 64807, 'Dieburg').
+kunde(105, 'Laaber', 'Kochstr. 23a', 64291, 'Darmstadt').
+kunde(113, 'Bozdag', 'Kostheimerstr. 18', 64354, 'Reinheim').
